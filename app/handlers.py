b632db4953fb09376e90eccae161e602e3219941
Bulo4kaskoricej <bulo4ka.s.koricej@gmail.com>
2026-02-17 21:13:30 +0300

    added gif on waiting

--- app/handlers.py
+++ app/handlers.py
@@ -9,7 +9,7 @@ from app.llm_client import ask_llm
 from app.prompts import*
 
 router = Router()
-
+WAIT_MESSAGE_GIF_URL="https://media1.tenor.com/m/OuPsTzfoh6cAAAAd/%D1%82%D0%B0%D0%BA%D0%B8%D0%B7%D0%B0%D0%BF%D0%B8%D1%88%D0%B5%D0%BC-%D0%B7%D0%B0%D0%BF%D0%B8%D1%88%D0%B5%D0%BC.gif"
 
 class CreatecharacterStates(StatesGroup):
     userPrompt = State()
@@ -28,10 +28,12 @@ async def create_character(message: Message, state: FSMContext):
 @router.message(CreatecharacterStates.userPrompt)
 async def give_character(message: Message, state: FSMContext):
     userPrompt = message.text.strip()
+    thinking_msg = await message.answer_animation(animation=WAIT_MESSAGE_GIF_URL,caption="Думаю над персонажем…")
     try:
         result = await ask_llm(userPrompt, CREATE_CHARACTER_PROMPT)
     except Exception as e:
         await message.answer(f"Ошибка при обращении к модели: {e}")
         return
+    await thinking_msg.delete()
     await message.answer(result)
     await state.clear()
